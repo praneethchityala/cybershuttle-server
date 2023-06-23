fa059af1700423ae6809b2895d3b8a0d6e84cfcc
Praneeth Chityala <praneethchityala44@gmail.com>
2023-06-23 00:41:08 -0400

    adding app-server

---
+++ app-server/cybershuttle-app-service/src/main/java/org/cybershuttle/appserver/AppServerApplication.java
@@ -0,0 +1,14 @@
+package org.cybershuttle.appserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppServerApplication.class, args);
+	}
+
+}
