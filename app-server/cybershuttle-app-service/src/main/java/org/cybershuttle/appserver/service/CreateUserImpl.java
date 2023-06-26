390811881dd931e2ed744446a6740b8979790561
Praneeth Chityala <praneethchityala44@gmail.com>
2023-06-26 19:23:45 -0400

    login and signup implementation

--- app-server/cybershuttle-app-service/src/main/java/org/cybershuttle/appserver/service/CreateUserImpl.java
+++ app-server/cybershuttle-app-service/src/main/java/org/cybershuttle/appserver/service/CreateUserImpl.java
@@ -10,24 +10,45 @@ public class CreateUserImpl extends UserServiceGrpc.UserServiceImplBase {
     @Override
     public void createUser(CreateUserRequest request, StreamObserver<CreateUserResponse> responseObserver) {
         String newUser = request.getNewUser().toString();
+        System.out.println("new user");
         System.out.println(newUser);
 
-        CreateUserResponse createUserResponse = CreateUserResponse.newBuilder()
-                .setId(101)
-                .build();
+        CreateUserResponse createUserResponse = CreateUserResponse.newBuilder().setId(101).build();
 
         responseObserver.onNext(createUserResponse);
         responseObserver.onCompleted();
     }
 
     @Override
-    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+    public void createToken(CreateTokenRequest request, StreamObserver<CreateTokenResponse> responseObserver) {
+        String user = request.getLogin().toString();
+        System.out.println("Creating token for");
+        System.out.println(user);
 
-        System.out.println("I got a ping");
+        CreateTokenResponse createTokenResponse = CreateTokenResponse.newBuilder().setToken("qwerty1234567890").build();
 
-        HelloReply helloReply = HelloReply.newBuilder().setMessage("I am up and running").build();
+        responseObserver.onNext(createTokenResponse);
+        responseObserver.onCompleted();
+    }
 
-        responseObserver.onNext(helloReply);
+    @Override
+    public void isAuthenticated(IsAuthenticatedRequest request, StreamObserver<IsAuthenticatedResponse> responseObserver) {
+
+        String token = request.getToken().toString();
+        System.out.println(token);
+
+        IsAuthenticatedResponse isAuthenticatedResponse = IsAuthenticatedResponse.newBuilder()
+                .setOk(true)
+                .setUser(User.newBuilder()
+                        .setId(101)
+                        .setEmail("test@cybershuttle.org")
+                        .setFirstName("Bob")
+                        .setLastName("Richardson")
+                        .setUsername("BobRich")
+                        .build())
+                .build();
+
+        responseObserver.onNext(isAuthenticatedResponse);
         responseObserver.onCompleted();
     }
 }
